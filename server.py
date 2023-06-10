51c286e1e638f6afa0282069c7ec15b91636e068
ArtemJustArtem <ashkyl97@gmail.com>
2023-06-10 12:32:27 +0300

    First commit in chat_divided

--- server.py
+++ server.py
@@ -6,10 +6,18 @@ from datetime import datetime
 import time
 
 app = Flask(__name__)
-messages = [
-    {'from': 'Mike', 'to': 'Jack', 'text': 'Hi, wanna hang out today?', 'time': time.time()},
-    {'from': 'Jack', 'to': 'Mike', 'text': 'Sorry, have to prepare for my finals((', 'time': time.time()}
-]
+
+chats = {
+    'testing': {
+        'length': 2,
+        'members': {'Mike', 'Jack'},
+        'messages': [
+            {'id': 0, 'from': 'Mike', 'text': 'Hi, wanna hang out today?', 'time': time.time()},
+            {'id': 1, 'from': 'Jack', 'text': 'Sorry, have to prepare for my finals((', 'time': time.time()}
+        ]
+    }
+}
+
 users = ['Mike', 'Jack']
 
 
@@ -34,72 +42,95 @@ def status():
 
 
 @app.route("/connect")
-def check_user():
-    """
-    The function that checks if user is registered.
-    If not the function can register them if necessary
-    """
+def register():
     try:
         user = request.args['name']
-        signin = request.args['signin']
     except:
         abort(400)
-    if user in users:
+    if not isinstance(user, str) or not user or len(user) > 1024:
+        abort(400)
+    if user not in users:
+        users.append(user)
+    return {'status': 'true'}
+
+
+@app.route("/adduser")
+def add_user():
+    try:
+        chat = request.args['chat']
+        user = request.args['user']
+    except:
+        abort(400)
+    for token in [user, chat]:
+        if not isinstance(token, str) or not token or len(token) > 1024:
+            abort(400)
+    if chat in chats:
+        chats[chat]['members'].add(user)
         return {'status': 'true'}
     else:
-        if signin == 'true':
-            users.append(user)
-            return {'status': 'true'}
-        else:
-            return {'status': 'false'}
+        return {'status': 'false'}
+
+
+@app.route("/addchat")
+def add_chat():
+    try:
+        chat = request.args['chat']
+        user = request.args['user']
+    except:
+        abort(400)
+    for token in [user, chat]:
+        if not isinstance(token, str) or not token or len(token) > 1024:
+            abort(400)
+    if chat in chats:
+        return {'status': 'false'}
+    else:
+        chats[chat] = {
+            'length': 0,
+            'members': {user},
+            'messages': {}
+        }
+        return {'status': 'true'}
 
 
 @app.route("/send", methods=['POST'])
 def send_view():
-    """
-    The function that adds the message
-    """
-    from_user = request.json.get('from')
-    to_user = request.json.get('to')
+    user = request.json.get('from')
+    chat = request.json.get('chat')
     text = request.json.get('text')
-
-    for token in [from_user, to_user, text]:
+    for token in [user, chat, text]:
         if not isinstance(token, str) or not token or len(token) > 1024:
             abort(400)
-
-    messages.append({'from': from_user, 'to': to_user, 'text': text, 'time': time.time()})
-    return {'status': True}
-
-
-def filter_messages(items, from_user, to_user):
-    """
-    Helping function that filters messages according to user's names
-    :param items: List of messages
-    :param from_user: Name of the user who wrote the message
-    :param to_user: Name of the user to whom the message was written
-    :return: The list of filtered messages
-    """
-    result = []
-
-    for item in items:
-        if item['from'] in [from_user, to_user] and item['to'] in [from_user, to_user]:
-            result.append(item)
-
-    return result
+    if chat in chats:
+        room = chats[chat]
+        if user in room['members']:
+            room['messages'].append({'id': room['length'], 'from': user, 'text': text, 'time': time.time()})
+            room['length'] += 1
+            return {'status': 'true'}
+        else:
+            return {'status': 'false'}
+    else:
+        return {'status': 'false'}
 
 
 @app.route("/messages")
 def messages_view():
-    """
-    The function that returns messages necessary to view
-    """
     try:
-        from_user = request.args['from']
-        to_user = request.args['to']
+        chat = request.args['chat']
+        user = request.args['user']
     except:
         abort(400)
-    filtered_messages = filter_messages(messages, from_user, to_user)
-    return {'messages': filtered_messages}
+    for token in [user, chat]:
+        if not isinstance(token, str) or not token or len(token) > 1024:
+            abort(400)
+    if chat in chats:
+        room = chats[chat]
+        if user in room['members']:
+            messages = room['messages']
+            return {'status': 'true', 'messages': messages}
+        else:
+            return {'status': 'false', 'messages': []}
+    else:
+        return {'status': 'false', 'messages': []}
 
 
 app.run()
