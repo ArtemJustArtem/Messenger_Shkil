8fd10b74323d5d5f04180a0721482a6f5ba49ee4
ArtemJustArtem <ashkyl97@gmail.com>
2023-06-10 12:40:25 +0300

    Second commit in chat_divided

--- reciever.py
+++ reciever.py
@@ -13,19 +13,20 @@ def format_message(item):
     :return: Formatted message
     """
     from_user = item['from']
-    to_user = item['to']
     text = item['text']
     date_time = datetime.fromtimestamp(item['time']).strftime('%H:%M:%S %d.%m.%Y')
-    return "from: {}\nto: {}\ntime: {}\ntext: {}\n".format(from_user, to_user, date_time, text)
+    return "from: {}\ntime: {}\ntext: {}\n".format(from_user, date_time, text)
 
 
 while True:
-    user_from = input("From: ")
-    user_to = input("To: ")
+    chat = input("Chat: ")
+    user = input("User: ")
     print()
-    response = requests.get('http://127.0.0.1:5000/messages', params={'from': user_from, 'to': user_to})
-    messages = response.json()['messages']
-    for message in messages:
-        print(format_message(message))
-
+    response = requests.get('http://127.0.0.1:5000/messages', params={'chat': chat, 'user': user})
+    if response.json()['status'] == 'false':
+        print('Something went wrong!')
+    else:
+        messages = response.json()['messages']
+        for message in messages:
+            print(format_message(message))
     time.sleep(1)
